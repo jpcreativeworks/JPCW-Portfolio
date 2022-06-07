0e1e842a868d52ef264bc44d0812d1664c75ce65
jpcreativeworks <jp.graphics.011@gmail.com>
2022-06-07 12:05:27 -0700

    correct spelling errors

--- jpcw-portfolio/src/components/About.js
+++ jpcw-portfolio/src/components/About.js
@@ -17,7 +17,7 @@ export default function About() {
                 <div className='card col-12 col-md-8 '>
                     <div className="card-body">
                         <h1>Hello.</h1>
-                        <p>JPCW is a one stop shop for your digital development needs. Working with the digital arts for over 10 years, growing and adapting with the technological changes but always looking towards the horizon and thinking out side the box. </p>
+                        <p>JPCW is a one stop shop for your digital development needs. Working with the digital arts for over 10 years, growing and adapting with the technological changes but always looking towards the horizon and thinking outside the box. </p>
                         
                     </div>
                 </div> 
@@ -28,7 +28,7 @@ export default function About() {
                 <img src={Camera} alt="Photography-Logo" style={ { "width":"100%"} }></img>
                  <div className="container">
                     <h2>Photography</h2>              
-                    <p>Digital and Film Photography, Photo Editing, and Wedding, Portrait, Family, Baby, Realestate Photography as well as Videography</p>              
+                    <p>Digital and Film Photography, Photo Editing, Wedding, Portrait, Family, Baby, Realestate Photography as well as Videography.</p>              
                 </div>
                 </div>
             
@@ -37,7 +37,7 @@ export default function About() {
             <img src={Web} alt="Web-development-Logo" style={ { "width":"100%"} }></img>
             <div className="container">
                 <h2>Web Development</h2>
-                <p>Recently Graduated from the University of Arizona's Full Stack Web Development Code Bootcamp and egar to hit the ground running with your next web application</p>
+                <p>Recently graduated from the University of Arizona's Full Stack Web Development Code Bootcamp and eager to hit the ground running with your next web application.</p>
             </div>
             </div>
         
@@ -47,7 +47,7 @@ export default function About() {
           <img src={Graphics} alt="Graphic-Design-Logo" style={ { "width":"100%"} }></img>
             <div className="container">
               <h2>Graphic Design</h2>              
-              <p>Do you need a unique logo for your business? Maybe a special vector file to help you stand out from the crowd? Well look no further! Just contact me about your graphic art needs and we will get the ball rolling on your next idea</p>                            
+              <p>Do you need a unique logo for your business? Maybe a special vector file to help you stand out from the crowd? Well look no further! Send a message about your graphic art needs and we will get the ball rolling on your next idea.</p>                            
             </div>
           </div>
         
--- jpcw-portfolio/src/components/Project.js
+++ jpcw-portfolio/src/components/Project.js
@@ -25,7 +25,7 @@ export default function Project() {
             <div className="card col-12 col-md-4" style={{"max-width": "25rem", "margin-right": "2rem"}}>                
                 <div className="card-body">
                     <h5 className="card-title">Shady Pines Cinema</h5>
-                    <p className="card-text">This is a movie search inspired by the Golden Girls. Our group wanted anyone who is in their older years to look up a movie and save it to their favorites to watch later. Open in Chrome for optimal use. <a href='https://jpcreativeworks.github.io/the-Golden-Group-Project/'>Click Here</a> </p>
+                    <p className="card-text">This is a movie search inspired by 'The Golden Girls'. Our group wanted anyone in their older years to be able look up a movie and save it to their "favorites" to watch later. Open in Chrome for optimal use. <a href='https://jpcreativeworks.github.io/the-Golden-Group-Project/'>Click Here</a> </p>
                     <p className="card-text"><small className="text-muted">Golden Group Collaborators: Jenn Bencriscutto, Lawson Vanderpool, Brant Martin and Samantha Lord</small></p>
                 </div>
                 <img src={Spc} className="card-img-bottom" alt="card-img-btm"></img>
@@ -34,7 +34,7 @@ export default function Project() {
                 <img src={Iven} className="card-img-top" alt="card-img-top"></img>               
                 <div className="card-body">
                     <h5 className="card-title">iVentory</h5>
-                    <p className="card-text">This app is for accountants to access prop houses in their area to request items for movies, Our client Apple Cinema asked for an updated website to speed up their workflow and prevent double spending on Props for movies. Open in Chrome for optimal use. <a href='https://datababes-iventory.herokuapp.com/'>Click Here</a> </p>
+                    <p className="card-text">This app is for accountants to access prop houses in their area to request items for movies. Our client Apple Cinema asked for an updated website to speed up their workflow and prevent double spending on props for movies. Open in Chrome for optimal use. <a href='https://datababes-iventory.herokuapp.com/'>Click Here</a> </p>
                     <p className="card-text"><small className="text-muted">Datababes Collaborators: Jenn Bencriscutto, Samantha Lord and Britinie Estime</small></p>
                 </div>
                 
@@ -43,7 +43,7 @@ export default function Project() {
                                
                 <div className="card-body">
                     <h5 className="card-title">Password Generator</h5>
-                    <p className="card-text">This is a password generator to make secure passwords. In today's day and age you can always use a secure password generator! Open in Chrome for optimal use. <a href='https://jpcreativeworks.github.io/Jenn-Password-Generator/'>Click Here</a> </p>
+                    <p className="card-text">This is a password generator to make secure passwords. In today's day and age, you can always use a secure password generator! Open in Chrome for optimal use. <a href='https://jpcreativeworks.github.io/Jenn-Password-Generator/'>Click Here</a> </p>
                     <p className="card-text"><small className="text-muted">Contributors: UofA and Trilogy Learning</small></p>
                 </div>
                 <img src={Pwdg} className="card-img-bottom" alt="card-img-btm"></img>
@@ -52,7 +52,7 @@ export default function Project() {
             <img src={Ubs} className="card-img-top" alt="card-img-top"></img>               
                 <div className="card-body">
                     <h5 className="card-title">The Ultimate Book Search</h5>
-                    <p className="card-text">This is an amazing book search, you can make your own account and search for books and save your favorites! Open in Chrome for optimal use. <a href='https://the-ultimate-book-search.herokuapp.com/'>Click Here</a> </p>
+                    <p className="card-text">This is an amazing book search, you can make your own account, search for books, and save your favorites! Open in Chrome for optimal use. <a href='https://the-ultimate-book-search.herokuapp.com/'>Click Here</a> </p>
                     <p className="card-text"><small className="text-muted">Contributors: UofA and Trilogy Learning</small></p>
                 </div>
                 
@@ -61,7 +61,7 @@ export default function Project() {
                         
                 <div className="card-body">
                     <h5 className="card-title">Text Editor App</h5>
-                    <p className="card-text">This is an amazing text editor app that you can install the app and have it running on your desktop whenever you need it! Open it in chrome for optimal use. <a href='https://text-editor-pwd.herokuapp.com/'>Click Here</a> </p>
+                    <p className="card-text">This is an amazing text editor app that you can install and have it running on your desktop whenever you need it! Open it in chrome for optimal use. <a href='https://text-editor-pwd.herokuapp.com/'>Click Here</a> </p>
                     <p className="card-text"><small className="text-muted">Contributors: UofA and Trilogy Learning</small></p>
                 </div>
                 <img src={Jate} className="card-img-bottom" alt="card-img-btm"></img>    
@@ -70,7 +70,7 @@ export default function Project() {
             <img src={Wfu} className="card-img-top" alt="card-img-top"></img>               
                 <div className="card-body">
                     <h5 className="card-title">Weather4U</h5>
-                    <p className="card-text">This weather application was my first time working with an API and bootstrap. Had a lot of fun with this project. <a href='https://jpcreativeworks.github.io/Jenn-Weather4U/'>Click Here</a> to get the weather!</p>
+                    <p className="card-text">This weather application was my first time working with an API and Bootstrap. Had a lot of fun with this project. <a href='https://jpcreativeworks.github.io/Jenn-Weather4U/'>Click Here</a> to get the weather!</p>
                     <p className="card-text"><small className="text-muted">Contributors: UofA and Trilogy Learning</small></p>
                 </div>
                 
@@ -88,7 +88,7 @@ export default function Project() {
             <img src={Appcard} className="card-img-top" alt="card-img-top"></img>               
                 <div className="card-body">
                     <h5 className="card-title">AppCard</h5>
-                    <p className="card-text">While I was interning for Yellow Brick Creative studio I was able to work with a local cafe to impliment their AppCard Campaign</p>
+                    <p className="card-text">While I was interning for Yellow Brick Creative Studio I was able to work with a local cafe to impliment their AppCard Campaign</p>
                     <p className="card-text"><small className="text-muted">Contributors: Yellow Brick Creative Studio, Northwest Cafe, Chronicle Cafe</small></p>
                 </div>
                 
@@ -97,7 +97,7 @@ export default function Project() {
                         
                 <div className="card-body">
                     <h5 className="card-title">Engagement Photos</h5>
-                    <p className="card-text">While living in Saint Louis I was able to photograph this amazing session to capture the love<a href='../img/engagement-photos.png'>Click Here</a> </p>
+                    <p className="card-text">While living in Saint Louis I was able to photograph this amazing session to capture the love. <a href='../img/engagement-photos.png'>Click Here</a> </p>
                     <p className="card-text"><small className="text-muted">Contributors: Sherise Kral and her Husband</small></p>
                 </div>
                 <img src={Marry} className="card-img-bottom" alt="card-img-btm"></img>    
@@ -115,7 +115,7 @@ export default function Project() {
             <img src={Cafep} className="card-img-top" alt="card-img-top"></img>               
             <div className="card-body">
                     <h5 className="card-title">Northwest Cafe Food Advertisment</h5>
-                    <p className="card-text">While interning with YBCS I was able to work with Northwest Cafe to photograph their food and create this Poster that was hung in the window of their store.<a href='../img/Cafe-Poster.png'>Click Here</a> </p>
+                    <p className="card-text">While interning with YBCS I was able to work with Northwest Cafe to photograph their food and create this poster that was hung in the window of their store. <a href='../img/Cafe-Poster.png'>Click Here</a> </p>
                     <p className="card-text"><small className="text-muted">Contributors: Yellow Brick Creative Studio, Northwest Cafe</small></p>
                 </div>
             </div>
@@ -123,7 +123,7 @@ export default function Project() {
                         
                 <div className="card-body">
                     <h5 className="card-title">Photo Edit Study</h5>
-                    <p className="card-text">While in college at McDaniel College, I was able to put together these really cool images of myself painted in a masterpieces.</p>
+                    <p className="card-text">While at McDaniel College, I was able to put together these really cool images of myself painted in masterpieces.</p>
                     <p className="card-text"><small className="text-muted">Contributors: Photoshop</small></p>
                 </div>
                 <img src={Master} className="card-img-bottom" alt="card-img-btm" ></img>    
@@ -133,7 +133,7 @@ export default function Project() {
                         
                 <div className="card-body">
                     <h5 className="card-title">VAMP Tour Posters</h5>
-                    <p className="card-text">I was lucky enough to have the opportunity to work with VAMP, a collective of performing Drag Queens and Ghols based in Tucson, AZ. I created these posters for their Spooky B*tchs Tour. Check out their instagram <a href='https://www.instagram.com/revamped.drag/'>here</a></p>
+                    <p className="card-text">I was lucky enough to have the opportunity to work with VAMP, a collective of performing Drag Queens and Ghouls based in Tucson, AZ. I created these posters for their Spooky B*tchs Tour. Check out their instagram <a href='https://www.instagram.com/revamped.drag/'>here</a>.</p>
                     <p className="card-text"><small className="text-muted">Contributors: VAMP</small></p>
                 </div>
                 <img src={Vamp} className="card-img-bottom" alt="card-img-btm"></img>    
@@ -150,7 +150,7 @@ export default function Project() {
                         
                 <div className="card-body">
                     <h5 className="card-title">JPCW Previous Website</h5>
-                    <p className="card-text">Finishing up the bootcamp I think it is important to see where you have come from. Check out my first Portfolio website <a href='https://jpcreativeworks.github.io/Jenn-Portfolio/'>here</a> and see how far I have come as a web Developer and Designer!</p>
+                    <p className="card-text">Finishing up the bootcamp I think it is important to see where you have come from. Check out my first portfolio website <a href='https://jpcreativeworks.github.io/Jenn-Portfolio/'>here</a> and see how far I have come as a web developer and designer!</p>
                     <p className="card-text"><small className="text-muted">Contributors: Photoshop</small></p>
                 </div>
                 <img src={Jpcw} className="card-img-bottom" alt="card-img-btm" ></img>    
